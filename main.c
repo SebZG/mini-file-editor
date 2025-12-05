d711ef1461c5d21d54753cb87e26ace8227dd086
Sebastian Zapata Gutierrez <sebastian.zapata.g@gmail.com>
2025-12-05 14:46:52 +0000

    Add: main.c + compile tasks

---
+++ main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main(void)
+{
+    while (1)
+    {
+        printf("\nMini File Editor\n"
+               "1) Create new file\n"
+               "2) Append to file\n"
+               "3) Read file\n"
+               "4) Exit"
+               "choose: ");
+
+        char choice = getchar();
+        while (getchar() != '\n'); // flush
+
+        switch (choice)
+        {
+        case '1':
+            create_file();
+            break;
+        case '2':
+            append_file();
+            break;
+        case '3':
+            read_file();
+            break;
+        case '4':
+            log_action("Eciting editor");
+            return 0;
+        default:
+            printf("Invalid option.\n");
+        }
+    }
+
+    return 0;
+}
